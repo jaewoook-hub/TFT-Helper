fb4d98192c521079137765127646619f60fdb8ae
Richard Ok <jaeok2017@u.northwestern.edu>
2020-11-04 11:49:40 -0600

    Routes to 3rd Party API complete

--- Server/server.js
+++ Server/server.js
@@ -6,6 +6,8 @@ app.use(express.json());
 app.use(cors());
 app.options('*', cors());
 const path = require('path');
+const LEAGUE = require('./leagueoflegends.js');
+const fetch = require('node-fetch');
 
 const Schemas = require('./DB/Schemas.js');
 
@@ -26,5 +28,33 @@ app.get('/api/champion/:name', function(req, res) {
     res.json(data);
   })
 });
+
+app.get('/api/summoner/:summonerName', (req, res) => {
+  let name = req.params.summonerName;
+  let newUrl = `https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/${name}?api_key=${LEAGUE}`;
+
+  fetch(newUrl)
+  .then(res => res.json())
+  .then(data => {
+     res.send({ data });
+  })
+  .catch(err => {
+     res.redirect('/error');
+  });
+})
+
+app.get('/api/summonerInfo/:encryptedSummonerId', (req, res) => {
+  let summonerId = req.params.encryptedSummonerId;
+  let newUrl = `https://na1.api.riotgames.com/tft/league/v1/entries/by-summoner/${summonerId}?api_key=${LEAGUE}`;
+
+  fetch(newUrl)
+  .then(res => res.json())
+  .then(data => {
+     res.send({ data });
+  })
+  .catch(err => {
+     res.redirect('/error');
+  });
+})
 
 module.exports = app;
